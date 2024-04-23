216779f7c4180a1669b3464f9bddc6be7220c687
Jennifer Simond <jenniasimond@gmail.com>
2024-04-23 00:26:30 -0400

    add navigation and routes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,1 +1,9 @@
 /* TODO - add your code to create a functional React component that renders a login form */
+
+const Login = () =>{
+    return (
+        <h1>Login 🔐</h1>
+    );
+}
+
+export default Login
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,1 +1,11 @@
 /* TODO - add your code to create a functional React component that renders a registration form */
+
+import React from "react";
+
+const Register = () => {
+   return (
+    <h1>Sign Up 🖊️</h1>
+   );
+}
+
+export default Register
